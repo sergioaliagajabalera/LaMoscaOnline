07ef60775657a22d09dcdaed19d6a0e3de2071d4
sergioaliagajabalera <sergio11aliaga@gmail.com>
2021-05-14 20:15:04 +0200

    update front shufled and divided...

--- server/models/Game.js
+++ server/models/Game.js
@@ -1,3 +1,5 @@
+const cardsgenerics=require('../mocks/cards/generics/cardsgenerics');
+
 module.exports = class Game {
 
 
@@ -10,6 +12,7 @@ module.exports = class Game {
       this.start_d=null;
       this.finish_d=null;
       this.winner=null;
+      this.cards=cardsgenerics;
     }
   
     set name(value) {
@@ -24,6 +27,18 @@ module.exports = class Game {
       var arrayplayers=[];
       this.players.forEach(a=>arrayplayers.push(a.player));
       return arrayplayers;
+    }
+
+    dividedcards(){
+      const shuffledArray = this.cards.sort((a, b) => 0.5 - Math.random()); //shuffled cards depend on if number random generated is negative or positive
+      this.cards=shuffledArray;
+      this.players.forEach(function(a){ //it give 4 cards for every player
+        for(let i=0; i<4; i++){
+          var card=this.cards[i]
+          this.cards.splice(i,1);
+          a.cards.push(card);
+        }
+      }.bind(this))
     }
 
   }
--- server/routes/games.js
+++ server/routes/games.js
@@ -13,7 +13,7 @@ exports.createGame=function(client,form,games) {//parameter "form" get values to
         let roomcode=player+''+Math.round(Math.random()*5999);//generate code room or game
 
         var game=new Game(roomcode,map,sizeroom)
-        let addplayer={player:player,rol:'admin',cards:[]}
+        let addplayer={player:player,rol:'admin',cards:new Array()}
         game.players.push(addplayer);
         games.push(game);
         console.log(games);
@@ -35,7 +35,7 @@ exports.joinGame=function(client,io,form,games) {//parameter "form" get values t
 
     var game=games.find(a=>a.roomcode==roomcode);
     if(game.length!=0) {
-      let addplayer={player:player,rol:'guest',cards:[]}   
+      let addplayer={player:player,rol:'guest',cards:new Array()}   
       game.players.push(addplayer);  
       console.log(games);
       client.join(roomcode);
@@ -59,7 +59,7 @@ exports.startGame=function(io,form,games) {//parameter "form" get values to   so
       let data= new Date();
       let hour=data.getHours()+':'+data.getMinutes()+':'+data.getSeconds;
       game.start_d=hour;
-      console.log(games);
+      game.dividedcards();
       console.log(io.sockets.adapter.rooms);
       console.log('start game');
       io.to(roomcode).emit('startsuccessful','Start Game');
@@ -77,7 +77,7 @@ exports.getstatusGame=function(client,io,form,games) {//parameter "form" get val
     client.username=player;
     console.log(io.sockets.adapter.rooms);
     console.log("get status");
-    console.log(game);
+    console.log(game.players[0].cards);
     let form={'game':game,'players':game.getAllplayers()}
     io.to(roomcode).emit('getstatussuccessful',form);
   }   
